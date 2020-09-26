d290f9013eedb66198e0374b43be97da175e60d8
csaybar <csaybar@gmail.com>
2020-09-26 23:07:13 +0200

    manually temporal step

--- src/cs2_02_image_creation.R
+++ src/cs2_02_image_creation.R
@@ -25,18 +25,28 @@ source("src/utils.R")
 ee_Initialize("csaybar", drive = TRUE)
 
 # Load potential points
-local_cloudsen2_points <- read_sf("data/cloudsen2.geojson") %>% arrange(type)
-create_potential_prob <- get_prob_by_class(local_cloudsen2_points) # potential points
-local_cloudsen2_points$potential_probability <-  create_potential_prob
+local_cloudsen2_points <- read_sf("data/cloudsen2.geojson") %>%
+  arrange(type) %>%
+  get_prob_by_class() # potential points
 
-# index <- 30
-for (index in 1:10) {
+for (index in seq_len(nrow(local_cloudsen2_points))) {
   cloudsen2_row <- local_cloudsen2_points[index,]
-  dataset_creator_chips(
+  select_dataset_thumbnail_creator(
     cloudsen2_row = cloudsen2_row,
-    bands = "B.*|probability|SCL",
+    n_images = 50,
     kernel_size = c(255, 255),
     data_range = c("2019-01-01", "2020-07-31"),
     output = "results/"
   )
 }
+
+# for (index in 1:10) {
+#   cloudsen2_row <- local_cloudsen2_points[index,]
+#   dataset_creator_chips(
+#     cloudsen2_row = cloudsen2_row,
+#     bands = "B.*|probability|SCL",
+#     kernel_size = c(255, 255),
+#     data_range = c("2019-01-01", "2020-07-31"),
+#     output = "results/"
+#   )
+# }
